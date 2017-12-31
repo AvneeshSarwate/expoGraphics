cccdbf5c4600116e3c978251b57ad26bbf4ab959
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-12-30 22:46:57 -0500

    PanResponder skeleton working

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import Expo from 'expo';
 import React from 'react';
+import PanResponder from 'react-native';
+// import React, {PanResponder} from 'react-native';
 
 const Dimensions = require('Dimensions');
 const win = Dimensions.get('window');
 
 var shaderText = `
+// stuff outside main() from Shawn Lawson's The Force - https://github.com/shawnlawson/The_Force
 precision lowp float;
 
 uniform vec2      resolution;
@@ -399,7 +402,7 @@ vec3 swirl(float time2, vec2 stN){
 void main () {
     vec2 stN = uvN();
     
-    float timex = time / 5.; // + mouse.x / resolution.x + mouse.y / resolution.y;
+    float timex = time / 5. + mouse.x / resolution.x + mouse.y / resolution.y;
 
     stN = rotate(vec2(0.5+sin(timex)*0.5, 0.5+cos(timex)*0.5), stN, sin(timex));
     
@@ -474,23 +477,61 @@ shaderText
   var timeVal = 0;
 
   function renderLoop() {
+    requestAnimationFrame(renderLoop);
     gl.uniform1f(time, timeVal += .2/6);
     //console.log(time);
     gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
     gl.endFrameEXP();
-    requestAnimationFrame(renderLoop);
   }
 
   renderLoop();
-
 }
 
 export default class App extends React.Component {
+  componentWillMount() {
+    console.log("COMPONENT WILL MOUNT", PanResponder.PanResponder);
+    this._panResponder = PanResponder.PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+
+        // gestureState.d{x,y} will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      },
+    });
+    console.log("PANRESPONDER MADE");
+  } 
   render() {
     return (
       <Expo.GLView
         style={{ flex: 1 }}
         onContextCreate={onGLContextCreate}
+        // {...this._panResponder.panHandlers}
       />
     );
   }
